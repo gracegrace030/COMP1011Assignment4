0083cc6e7106bc209ca44cfad4db4644a19f18c0
gracegrace030 <113151468+gracegrace030@users.noreply.github.com>
2023-07-19 00:02:15 -0400

    Format

--- studentGrade.sql
+++ studentGrade.sql
@@ -5,41 +5,42 @@ USE COMP1011AS4;
 
 
 DROP TABLE IF EXISTS Students;
-CREATE TABLE Students(
-    sid int(9) AUTO_INCREMENT PRIMARY KEY,
-    firstName varchar(50) NOT NULL,
-    lastName varchar(50) NOT NULL,
-    program varchar(30)NOT NULL,
+CREATE TABLE Students (
+    sid INT(9) AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    program VARCHAR(30) NOT NULL,
     intakeYear YEAR,
-    intakeSeason enum("Winter", "Fall", "Summer"),
-    graduateYear year
+    intakeSeason ENUM('Winter', 'Fall', 'Summer'),
+    graduateYear YEAR
 );
 
 DROP TABLE IF EXISTS Teachers;
-CREATE TABLE Teachers(
-    tid int(9) AUTO_INCREMENT PRIMARY KEY,
-    firstName varchar(50) NOT NULL,
-    lastName varchar(50) NOT NULL
-    -- department varchar(30)NOT NULL
+CREATE TABLE Teachers (
+    tid INT(9) AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS Courses;
-CREATE TABLE Courses(
-    code char(16) PRIMARY KEY, -- eg. COMP1011-23S-101
-    courseName varchar(100) NOT NULL,
-    tid int NOT NULL,
-    -- department varchar(50) NOT NULL,
-    academicSeason ENUM("Winter", "Fall", "Summer"),
-    academicYear year,
-    FOREIGN KEY (tid) REFERENCES Teachers(tid)
+CREATE TABLE Courses (
+    code CHAR(16) PRIMARY KEY,
+    courseName VARCHAR(100) NOT NULL,
+    tid INT NOT NULL,
+    academicSeason ENUM('Winter', 'Fall', 'Summer'),
+    academicYear YEAR,
+    FOREIGN KEY (tid)
+        REFERENCES Teachers (tid)
 );
 
 DROP TABLE IF EXISTS AcademicRecords;
-CREATE TABLE AcademicRecords(
-	recordId INT AUTO_INCREMENT PRIMARY KEY,
-    sid int(9) NOT NULL,
-    FOREIGN KEY (sid) REFERENCES Students(sid),
-    code char(16) NOT NULL,
-    FOREIGN KEY (code) REFERENCES Courses(code),
-    grade int NOT NULL
+CREATE TABLE AcademicRecords (
+    recordId INT AUTO_INCREMENT PRIMARY KEY,
+    sid INT(9) NOT NULL,
+    FOREIGN KEY (sid)
+        REFERENCES Students (sid),
+    code CHAR(16) NOT NULL,
+    FOREIGN KEY (code)
+        REFERENCES Courses (code),
+    grade INT NOT NULL
 );
