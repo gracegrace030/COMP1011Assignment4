a273e30635cb70a202ee1ed76ed235922d6ec4ed
gracegrace030 <113151468+gracegrace030@users.noreply.github.com>
2023-07-20 17:41:41 -0400

    Add Dummy data and change column name and order
    1. Column name changed from "recordId" to "rid"
    2. Added dummy date

--- studentGrade.sql
+++ studentGrade.sql
@@ -35,22 +35,62 @@ CREATE TABLE Courses (
 
 DROP TABLE IF EXISTS Student_Courses;
 CREATE TABLE Student_Courses (
-	cid CHAR(16)NOT NULL,
-    FOREIGN KEY (cid)
-        REFERENCES Courses (cid),
-    sid INT(9) NOT NULL,
+	sid INT(9) NOT NULL,
 	FOREIGN KEY (sid)
-        REFERENCES Students (sid)
+        REFERENCES Students (sid),
+    cid CHAR(16)NOT NULL,
+    FOREIGN KEY (cid)
+        REFERENCES Courses (cid)
 );
 
 DROP TABLE IF EXISTS AcademicRecords;
 CREATE TABLE AcademicRecords (
-    recordId INT AUTO_INCREMENT PRIMARY KEY,
-    grade INT NOT NULL,
+    rid INT AUTO_INCREMENT PRIMARY KEY,
     sid INT(9) NOT NULL,
     FOREIGN KEY (sid)
         REFERENCES Students (sid),
     cid CHAR(16) NOT NULL,
     FOREIGN KEY (cid)
-        REFERENCES Courses (cid)
+        REFERENCES Courses (cid),
+	
+    grade DECIMAL(4,1) NOT NULL
 );
+
+-- ============================================
+-- Dummy data for testing
+
+INSERT INTO
+	Students(firstName, lastName, program, intakeYear, intakeSeason, graduateYear)
+VALUE
+	("Abby", "Slater", "Computer Programming", 2020, "Winter", 2023),
+    ("Johnny", "Edwards", "Business Administration", 2022, "Fall", 2023);
+
+INSERT INTO
+	Teachers(firstName, lastName)
+VALUE
+	("Patrick", "Star"),
+    ("Sponge", "Bob");
+
+INSERT INTO
+	Courses(cid, courseName, tid, academicSeason, academicYear)
+VALUE
+	("COMP100022F30987", "Object Oriented Programming", 2, "Fall", 2022),
+    ("MATH100223W10001", "Mathematics of Finance", 1, "Winter", 2023);
+    
+INSERT INTO
+	Student_Courses(sid, cid)
+VALUE
+	(1, "COMP100022F30987"),
+    (2, "MATH100223W10001");
+    
+INSERT INTO
+	AcademicRecords(sid, cid, grade)
+VALUE
+	(1, "COMP100022F30987",90.6),
+    (2, "MATH100223W10001",100);
+    
+TABLE Students;
+TABLE Teachers;
+TABLE Courses;
+TABLE Student_Courses;
+TABLE AcademicRecords;
